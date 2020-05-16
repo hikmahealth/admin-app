76a5c83dbdd956c9acc534f0b919c92f4a998d30
Sam Brotherton <sbrother@gmail.com>
2020-05-15 22:32:44 -0600

    Use env var for password reset endpoint

--- src/components/PasswordReset.tsx
+++ src/components/PasswordReset.tsx
@@ -76,7 +76,7 @@ const PasswordReset = (props: any) => {
   };
 
   const changePassword = async (): Promise<any> => {
-    const response = await fetch(`http://demo-api.hikmahealth.org/admin_api/change_password`, {
+    const response = await fetch(`${process.env.REACT_APP_INSTANCE_URL}/admin_api/change_password`, {
       method: 'POST',
       headers: {
         Authorization: state.token
